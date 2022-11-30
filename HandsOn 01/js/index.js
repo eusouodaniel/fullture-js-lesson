4aba58f555320e2a76d41fec647a3bc7e8138887
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-11-30 20:47:18 -0300

    new: last lesson unit 1

--- HandsOn 01/js/index.js
+++ HandsOn 01/js/index.js
@@ -18,13 +18,19 @@ form.addEventListener("submit", (e) => {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let option = document.getElementById("options").value;
-    let radio = document.querySelector('input[name="option"]:checked').value
 
+    //primeiro valida se existe algo "checkado"
+    //pego o value do elemento
+    let radio = document.querySelector('input[name="option"]:checked');
+    let checkbox = document.querySelector('input[name="option"]:checked');
+    let textarea = document.getElementById("textarea").value;
     let usuario = {
         'email': email,
         'senha': password,
         'option': option,
-        'radio': radio
+        'radio': radio ? radio.value : null,
+        'checkbox': checkbox ? checkbox.value : null,
+        'textarea': textarea
     }
     verificaCampos(usuario)
 })
@@ -56,3 +62,33 @@ function verificaCampos(usuario) {
     
     return document.getElementById("mensagem").innerHTML = "Digite um nome";
 }
+
+
+//array
+let nomes = ['Daniel', 'Marcio', 'João', 'Lucas', 'Fernanda'];
+
+nomes.push('Pâmela')
+nomes.pop();
+nomes.unshift('Lucas')
+nomes.shift();
+nomes.sort();
+nomes.reverse();
+
+// console.table(nomes)
+nomes.forEach(nome => {
+    // console.log(nome)
+});
+
+let valores = [1, 2, 3, 4, 5];
+// let valores2 = valores.map(valor => valor * 2)
+// console.log(valores2)
+// let valores2 = valores.filter(valor => {
+//     return valor % 2 == 0
+// })
+// console.log(valores2)
+
+// previous, current
+let resultado = valores.reduce((result, valor) => {
+    return Number(result) + Number(valor);
+}, 0);
+console.log(resultado)
