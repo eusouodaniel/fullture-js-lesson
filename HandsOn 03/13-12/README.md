5719b3b41a1b4f73af6f8a42043c34aad026fd26
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-12-14 23:10:47 -0300

    new: lesson 14-12

--- HandsOn 03/13-12/README.md
+++ HandsOn 03/13-12/README.md
@@ -2,10 +2,10 @@
 
 - Concessionaria de carros
 
-objeto - carro - marca
-                 tipo
-                 modelo
-                 ano
+objeto - carro - marca - filtro
+                 tipo - filtro
+                 modelo - colocar filtro
+                 ano 
                  preco
          carros = array
 
@@ -17,7 +17,7 @@ eu tenho uma div no front
 eu quero popular essa div
 a informação tá no javascript
 
-//novo objetivo
+//novo objetivo - resolvido
 eu quero ter dois filtros
 Busca mais robusta/completa
 
@@ -25,7 +25,23 @@ Busca mais robusta/completa
 normalização dos dados
 
 //próxima aula
-Adicionar mais alguns filtros
-Enriquecer lista de carros
-Select mais escalável
-Cadastrar carros
+1- Adicionar mais alguns filtros
+   - Filtro pelo modelo
+   - Colocando outro input ou colocando outro select
+   - Entender como popular o select
+   - Select dinâmico - de acordo com marca selecionada
+   - Criar select - porém vazio
+   informação hard coded
+2- Criar condição para listar tudo - resolvido
+3- Enriquecer lista de carros - quando a gente estiver trabalhando com local/sessionStorage
+4- Select mais escalável - por hora despriorizado
+5- Cadastrar carros - cenário foi devidamente concluído
+6- Prática
+
+//Futuro
+- Entender se faz sentido utilizar .filter ao invês do if
+de - para (dicionário)
+altocusto - Alto custo
+mediocusto - Médio custo
+baixocusto - Baixo custo
+- Cenário de falha na busca
--- HandsOn 03/13-12/js/index.js
+++ HandsOn 03/13-12/js/index.js
@@ -1,3 +1,5 @@
+const TODOS = "todos";
+
 let marca = '';
 let tipo = '';
 
@@ -49,14 +51,31 @@ carros.forEach(carro => {
 let filtro = document.getElementById("filtro");
 filtro.addEventListener('change', (e) => {
     divCarros.innerHTML = "";
+
+    let selectModelo = document.getElementById("selectModelo");
+    selectModelo.innerHTML = "";
     carros.forEach(carro => {
         let marcaNormalizada = carro.marca.toLowerCase();
         let selectNormalizado = e.target.value.toString().toLowerCase();
         let tipoCarro = carro.tipo.toLowerCase();
-
         if (marcaNormalizada == selectNormalizado
             && tipoCarro.indexOf(tipo) != -1) {
+            
+            let option = `<option value="${carro.modelo.toLowerCase()}">${carro.modelo}</option>`
+            selectModelo.innerHTML += option;
+
             marca = selectNormalizado;
+            divCarros.innerHTML += `
+                <h3>${carro.marca}</h3>
+                <p>${carro.tipo}</p>
+                <p>${carro.modelo}</p>
+                <p>${carro.ano}</p>
+                <p>${carro.preco}</p>
+                <br>
+            `
+        } else if (selectNormalizado == TODOS) {
+            let option = `<option value="${carro.modelo.toLowerCase()}">${carro.modelo}</option>`
+            selectModelo.innerHTML += option;
             divCarros.innerHTML += `
                 <h3>${carro.marca}</h3>
                 <p>${carro.tipo}</p>
@@ -68,6 +87,25 @@ filtro.addEventListener('change', (e) => {
         }
     })
 });
+
+let modelo = document.getElementById("modelo");
+modelo.addEventListener('change', (e) => {
+    divCarros.innerHTML = "";
+    let seletNormalizado = e.target.value;
+    carros.forEach(carro => {
+        let modeloNormalizado = carro.modelo.toLowerCase();
+        if (seletNormalizado == modeloNormalizado) {
+            divCarros.innerHTML += `
+                <h3>${carro.marca}</h3>
+                <p>${carro.tipo}</p>
+                <p>${carro.modelo}</p>
+                <p>${carro.ano}</p>
+                <p>${carro.preco}</p>
+                <br>
+            `
+        }
+    })
+})
 
 let filtroTipo = document.getElementById("filtroTipo");
 filtroTipo.addEventListener('input', (e) => {
@@ -91,4 +129,25 @@ filtroTipo.addEventListener('input', (e) => {
             `
         }
     })
+})
+
+let form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let marca = document.getElementById("marca").value;
+    let tipo = document.getElementById("tipo").value;
+    let modelo = document.getElementById("modelo").value;
+    let ano = document.getElementById("ano").value;
+    let preco = document.getElementById("preco").value;
+
+    let carro = {
+        marca,
+        tipo,
+        modelo,
+        ano,
+        preco
+    }
+    form.reset();
+    alert("Carro foi cadastrado")
+    carros.push(carro)
 })
